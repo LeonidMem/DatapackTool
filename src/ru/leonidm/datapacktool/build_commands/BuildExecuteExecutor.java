187afce98a03936baaf433e40c4ed3473437f4d1
LeonidMem <wildhs.botm@gmail.com>
2021-10-19 19:05:42 +0500

    Added static BuildCommandExecutor.createAnonymousFunction()

--- src/ru/leonidm/datapacktool/build_commands/BuildExecuteExecutor.java
+++ src/ru/leonidm/datapacktool/build_commands/BuildExecuteExecutor.java
@@ -1,46 +1,21 @@
 package ru.leonidm.datapacktool.build_commands;
 
-import ru.leonidm.datapacktool.Utils;
 import ru.leonidm.datapacktool.entities.BuildCommandExecutor;
 
 import java.io.File;
-import java.io.FileOutputStream;
-import java.io.OutputStream;
-import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public class BuildExecuteExecutor implements BuildCommandExecutor {
 
-    private final static String sep = String.valueOf(Utils.getFileSeparator()).replace("\\", "\\\\");
-
     @Override
     public void execute(StringBuilder outFileBuilder, String[] args, String anonymousFunctionContent, File inFile, File outFile, List<File> anonymousFiles) throws Exception {
-
-        File anonymousFile = new File(outFile.getParentFile(), outFile.getName().substring(0, outFile.getName().length() - 11) + anonymousFunctionContent.hashCode() + ".mcfunction");
-        OutputStream outputStream = new FileOutputStream(anonymousFile);
-        outputStream.write(anonymousFunctionContent.getBytes(StandardCharsets.UTF_8));
-        outputStream.close();
-
-        anonymousFiles.add(anonymousFile);
-
-        String regex1 = sep + "data" + sep;
-
-        String namespace = inFile.getParent().split(regex1, 2)[1].split(sep, 2)[0];
-
-        String regex2 = regex1 + namespace + sep + "functions";
-
-        String directory = outFile.getParent().split(regex2, 2)[1].replace("\\", "/");
-        if(directory.startsWith("/")) directory = directory.substring(1);
-        if(!directory.equals("")) directory += "/";
+        String functionName = BuildCommandExecutor.createAnonymousFunction(inFile, outFile, anonymousFunctionContent, anonymousFiles);
 
         String line = String.join(" ", args);
         outFileBuilder.append("execute ")
                 .append(line.replace("{", "").strip())
                 .append(" run function ")
-                .append(namespace)
-                .append(':')
-                .append(directory)
-                .append(anonymousFile.getName(), 0, anonymousFile.getName().length() - 11)
+                .append(functionName)
                 .append('\n');
     }
 }
--- src/ru/leonidm/datapacktool/entities/BuildCommandExecutor.java
+++ src/ru/leonidm/datapacktool/entities/BuildCommandExecutor.java
@@ -1,9 +1,38 @@
 package ru.leonidm.datapacktool.entities;
 
+import ru.leonidm.datapacktool.Utils;
+
 import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
 import java.util.List;
 
 public interface BuildCommandExecutor {
 
     void execute(StringBuilder outFileBuilder, String[] args, String anonymousFunctionContent, File inFile, File outFile, List<File> anonymousFiles) throws Exception;
+
+    static String createAnonymousFunction(File inFile, File outFile, String anonymousFunctionContent, List<File> anonymousFiles) throws IOException {
+        String sep = String.valueOf(Utils.getFileSeparator()).replace("\\", "\\\\");
+
+        File anonymousFile = new File(outFile.getParentFile(), outFile.getName().substring(0, outFile.getName().length() - 11) + anonymousFunctionContent.hashCode() + ".mcfunction");
+        OutputStream outputStream = new FileOutputStream(anonymousFile);
+        outputStream.write(anonymousFunctionContent.getBytes(StandardCharsets.UTF_8));
+        outputStream.close();
+
+        anonymousFiles.add(anonymousFile);
+
+        String regex1 = sep + "data" + sep;
+
+        String namespace = inFile.getParent().split(regex1, 2)[1].split(sep, 2)[0];
+
+        String regex2 = regex1 + namespace + sep + "functions";
+
+        String directory = outFile.getParent().split(regex2, 2)[1].replace("\\", "/");
+        if(directory.startsWith("/")) directory = directory.substring(1);
+        if(!directory.equals("")) directory += "/";
+
+        return namespace + ":" + directory + anonymousFile.getName().substring(0, anonymousFile.getName().length() - 11);
+    }
 }
